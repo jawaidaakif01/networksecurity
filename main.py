b772f58f4936166962145bcb8e6d7f4482c39759
Aakif Jawaid <jawaid.aakif@gmail.com>
2025-12-27 12:31:33 +0530

    Data Transformation commit

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
+from networksecurity.components.data_transformation import DataTransformation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 import sys
 if __name__=="__main__":
@@ -14,12 +15,18 @@ if __name__=="__main__":
         dataingestionartifact=data_ingestion.initiate_data_ingestion()
         logging.info("data initiate completed")
         print(dataingestionartifact)
-        data_validation_config=DataValidationConfig(trainingpipelineconfig)
-        print(f"DEBUG: data_validation_config TYPE is: {type(data_validation_config)}")
+        data_validation_config=DataValidationConfig(trainingpipelineconfig)        
         data_validation=DataValidation(dataingestionartifact, data_validation_config)
         logging.info("Initiate the data validation")
         data_validation_artifact = data_validation.initiate_data_validation()
         logging.info("Data validation completed")
+        print(data_validation_artifact)
+        data_transformation_config=DataTransformationConfig(trainingpipelineconfig)
+        logging.info("Data transformation started")
+        data_transformation=DataTransformation(data_validation_artifact, data_transformation_config)
+        data_transformation_artifact = data_transformation.initiate_data_transformation()
+        print(data_transformation_artifact)
+        logging.info("data Transformation completed")
 
 
     except Exception as e:
